30239878d69df517550192d6eef7112e57306004
nearnshaw <earnshaw.nico@gmail.com>
2025-10-18 09:42:14 -0300

    clean up

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import { engine } from '@dcl/sdk/ecs'
 import { addPointerEvents, grabSystem } from './modules/grabSystem' 
 import { getPlayerID } from './modules/helpers'
 import { onPlayerExpressionObservable } from '@dcl/sdk/observables'
-import { cleanupSystem, spawnAvatarClone } from './modules/clones'
 import { addLights } from './modules/lights'
 import { setupUI } from './ui'
 import { resetGold } from './modules/gold'
@@ -16,11 +15,5 @@ export function main() {
     setupUI()
 	engine.addSystem(grabSystem)
     addLights()
-
-    engine.addSystem(cleanupSystem)
-
-    onPlayerExpressionObservable.add(({ expressionId }) => {
-      if (!expressionId) return
-      spawnAvatarClone(expressionId)
-    })
+ 
 }
